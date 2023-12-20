4b0f47cbf035c25962a4857d558435ea75fb0355
FirstFirstFirstFirst <hocrux1st.first@gmail.com>
2023-12-20 21:31:15 +0700

    second page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import WelcomePage from '@/components/WelcomePage'
+import WhyCarbonPage from '@/components/WhyCarbonPage'
 const page = () => {
   return (
     <div>
       <WelcomePage />
+      <WhyCarbonPage />
     </div>
   )
 }
