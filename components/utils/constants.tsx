0d86454f86351f6a6519326c65245e4ab3817833
BIGGY00 <bowbigzaza2014@gmail.com>
2023-12-28 15:14:11 +0700

    fix wallet admin

--- components/utils/constants.tsx
+++ components/utils/constants.tsx
@@ -1,4 +1,4 @@
 import abi from './Transaction.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0xe88313Ee264470b556B5386C7608324Df4329Ec6';
+export const contractAddress = '0xe8f39BbbE7c29F9AC2A0B71E8b14Da26d7fF182f';
